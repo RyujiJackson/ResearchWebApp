3d05cae0926f7008c4c73d376b00be4ce184f502
AMOR <77601937+RyujiJackson@users.noreply.github.com>
2024-04-18 12:02:54 +0900

    edit zoom code fix zoom to have at least useable function

--- static/ImageViewer.js
+++ static/ImageViewer.js
@@ -5,6 +5,10 @@ var zoomButton = document.getElementById("zoom-button");
 var canvasContainer = document.getElementById("canvas-container");
 var preventZoom = true;
 
+let currentZoom = 1;
+let minZoom = 0.8; 
+let maxZoom = 5; 
+let stepSize = 0.07;
 let scale = 1;
 let offsetX = 0;
 let offsetY = 0;
@@ -28,16 +32,48 @@ function toggleZoom() {
     }
 }
 
-canvasContainer.addEventListener('wheel', function(event) {
+canvas.addEventListener('wheel', function(event) {
     if (!preventZoom) {
+        /*
+        //old code to apply zoom
         event.preventDefault(); // Prevent default scrolling behavior
-        const deltaY = event.deltaY; // Get scroll delta (positive for zoom in, negative for zoom out)
-        scale += deltaY * 0.0004; // Adjust zoom amount based on scroll amount
+
+        const deltaY = event.deltaY;
+        const changeScale = (delta) => {
+            scale = Math.max(0.5, Math.min(1.5, scale * Math.exp(delta * 0.0004)));
+                
+        }
+        changeScale(deltaY)
+        
         applyZoom();
+        */
+        //new code to apply zoom
+        event.preventDefault(); // Prevent default scrolling behavior
+        let direction = event.deltaY > 0 ? -1 : 1; 
+        zoomImage(direction); 
+        
+        
     }
 });
 
-// Function to apply zoom
+//new function to apply zoom
+function zoomImage(direction) { 
+    let newZoom = currentZoom + direction * stepSize; 
+  
+    // Limit the zoom level to the minimum and maximum values 
+    if (newZoom < minZoom || newZoom > maxZoom) { 
+        return; 
+    } 
+  
+    currentZoom = newZoom; 
+  
+    // Update the CSS transform of the image to scale it 
+    canvas.style.transform = 'scale(' + currentZoom + ')'; 
+    sourceImage.style.transform = 'scale(' + currentZoom + ')'; 
+}
+
+/*
+// old Function to apply zoom
 function applyZoom() {
     canvas.style.transform = `scale(${scale})`;
     //canvas.style.transformOrigin = 'top left';
@@ -48,7 +84,7 @@ function applyZoom() {
     offsetX *= scale;
     offsetY *= scale;
     applyPan();
-}
+}*/
 
 function applyPan() {
     sourceImage.style.left = offsetX + 'px';

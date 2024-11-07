0bd0330e033d968da6550b014a4578bb8d2a3d69
AMOR <77601937+RyujiJackson@users.noreply.github.com>
2024-11-07 16:40:02 +0900

    create requirements.txt with pipreqs

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 UPLOAD_FOLDER = 'static/uploads/origin/'
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
